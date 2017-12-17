0840954e92276f88d71d6ecefcd304665a9d1b18
Do Tien Hai <do.tien.hai@framgia.com>
2017-12-17 20:33:25 +0700

    Implement basic login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,17 @@
 class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   before_save {email.downcase!}
-  validates :name,  presence: true, length: {maximum: 50}
-  validates :email, presence: true, length: {maximum: 255},
+  validates :name,  presence: true, length: {maximum: Settings.user.name_max_length}
+  validates :email, presence: true, length: {maximum: Settings.user.email_max_length},
     format: {with: VALID_EMAIL_REGEX},
     uniqueness: {case_sensitive: false}
   has_secure_password
-  validates :password, presence: true, length: {minimum: 6}
+  validates :password, presence: true,
+    length: {minimum: Settings.user.password_minimum_length}
+
+  def User.digest string
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+      BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
